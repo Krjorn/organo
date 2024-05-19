b5df71fde46c6270029081950fc594725eca3b86
Krjorn <christopher990172@gmail.com>
2024-05-19 03:48:52 -0300

    Change img src path in Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,23 +7,23 @@ const Footer = () => {
             <ul>
                 <li>
                     <a href="facebook.com" target="_blank">
-                        <img src="/images/fb.png" alt="" />
+                        <img src="./images/fb.png" alt="" />
                     </a>
                 </li>
                 <li>
                     <a href="twitter.com" target="_blank">
-                        <img src="/images/tw.png" alt="" />
+                        <img src="./images/tw.png" alt="" />
                     </a>
                 </li>
                 <li>
                     <a href="instagram.com" target="_blank">
-                        <img src="/images/ig.png" alt="" />
+                        <img src="./images/ig.png" alt="" />
                     </a>
                 </li>
             </ul>
         </section>
         <section>
-            <img src="/images/logo.png" alt="" />
+            <img src="./images/logo.png" alt="" />
         </section>
         <section>
             <p>
